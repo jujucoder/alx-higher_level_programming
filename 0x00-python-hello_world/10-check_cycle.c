298b72e02534deaa8ef002375fe766a5d55a113d
okputu julius <juliusstan10@gmail.com>
2023-01-04 18:23:58 +0000

    10 fix

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -3,17 +3,17 @@
 int check_cycle(listint_t *list)
 {
 	listint_t *temp;
-
 	
+	temp =  list->next->next;
 	
-	while(list && list->next)
+	while(list && list->next && temp)
 	{
-		temp = list;
-		list = list->next;
 		if (list == temp)
 		{
 			return(1);
 		}
+		list = list->next;
+		temp = temp->next;
 	}
 	
 	return (0);
