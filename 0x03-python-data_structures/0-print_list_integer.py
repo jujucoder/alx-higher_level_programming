c8ef249f57d6b7181f82944f722d19fcfded9231
okputu julius <juliusstan10@gmail.com>
2023-01-07 17:29:52 +0000

    task 0 fix

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
-    def print_list_integer(my_list=[]):
-        if len(my_list) != 0:
-            for i in range(0, len(my_list)):
-                print("{:d}".format(my_list[i]))
-            
+def print_list_integer(my_list=[]):
+    if len(my_list) != 0:
+        for i in range(0, len(my_list)):
+            print("{:d}".format(my_list[i]))            
