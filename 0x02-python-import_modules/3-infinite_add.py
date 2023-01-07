7825f2bf65bf5c6799da7a5c3533d21ef1725db3
okputu julius <juliusstan10@gmail.com>
2023-01-07 09:19:13 +0000

    task 3 fix

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,6 +3,6 @@ if __name__ == '__main__':
     from sys import argv
     total = 0
     for item in argv:
-        if item  != argv[0]:
+        if item != argv[0]:
             total += int(item)
     print(total)
