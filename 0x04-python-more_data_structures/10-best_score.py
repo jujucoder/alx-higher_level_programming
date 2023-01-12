a6f545fc17c642fea9cb8c549d8d7fe453c2499f
okputu julius <juliusstan10@gmail.com>
2023-01-12 19:18:46 +0000

    task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if not a_dictionary or max(a_dictionary) == int:
+    if not a_dictionary or type(a_dictionary[max(a_dictionary)]) != int:
         return None
     return max(a_dictionary)
