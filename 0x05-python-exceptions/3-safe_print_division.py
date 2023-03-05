27214f8f33074e8bdad18e3926264c891033b2b2
okputu julius <juliusstan10@gmail.com>
2023-03-05 21:46:07 +0100

    task 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,8 +3,8 @@ def safe_print_division(a, b):
 
     try:
         result = a/b
-        return result
     except Exception:
         return None
     finally:
-        print("Inside result: {}".format(a/b))
+        print("Inside result: {}".format(result))
+        return result
