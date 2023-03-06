4ce159295dcc25d9b71fb332fc82e5c4d838cade
okputu julius <juliusstan10@gmail.com>
2023-03-06 17:27:11 +0100

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+1. My first square
+    Contains an empty class that defines a square
+"""
+
+
+class Square:
+    """
+    base class
+    """
+    def __init__(self, size):
+        self.__size = size
