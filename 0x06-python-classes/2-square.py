821586288ee570f45a058bd21f297899d72e6727
okputu julius <juliusstan10@gmail.com>
2023-03-06 17:35:52 +0100

    task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+1. My first square
+    Contains an empty class that defines a square
+"""
+
+
+class Square:
+    """
+    base class
+    """
+    def __init__(self, size = 0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
