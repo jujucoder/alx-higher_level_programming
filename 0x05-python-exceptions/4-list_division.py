08e1409d783c3202c4911acf348f7b99112cf8a9
okputu julius <juliusstan10@gmail.com>
2023-03-06 10:34:35 +0100

    task 4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -11,3 +11,5 @@ def list_division(my_list_1, my_list_2, list_length):
             print("out of range")
         except (ValueError, TypeError):
             print("wrong type")
+        finally:
+            print(new_list)
