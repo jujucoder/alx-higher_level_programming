802269163182084b46ccda3191fa6ba304c68d81
okputu julius <juliusstan10@gmail.com>
2023-03-07 21:49:46 +0100

    task 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+module
+"""
+def is_integer_num(n):
+    """
+    checks if a number is a float or an int
+    """
+    if isinstance(n, int):
+        return True
+    if isinstance(n, float):
+        return True
+
+def add_integer(a, b=98):
+    """
+    adds integers
+    """
+    if not is_integer_num(a):
+        raise TypeError("a must be an integer")
+    if not is_integer_num(b):
+        raise TypeError("b must be an integer")
+    result = int(a) + int(b)
+    return result
+
