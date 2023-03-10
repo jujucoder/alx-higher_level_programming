e769aefbab3518f2f82ea2b337f9815f2466109a
okputu julius <juliusstan10@gmail.com>
2023-03-10 21:45:54 +0100

    task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -11,18 +11,19 @@ class Rectangle:
     """
 
     def __init__(self, width=0, height=0):
-        self.width = width
         self.height = height
+        self.width = width
+        
     
     @property
     def width(self):
         return self.__width
     @width.setter
     def width(self, width):
-        if width < 0:
-            raise ValueError("width must be >= 0")
         if not isinstance(width, int) and not isinstance(width, float):
             raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
         
         self.__width = width
 
@@ -32,9 +33,9 @@ class Rectangle:
         return self.__height
     @height.setter
     def height(self, height):
-        if height < 0:
-            raise ValueError("height must be >= 0")
         if not isinstance(height, int) and not isinstance(height, float):
             raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
         
         self.__height = height
